049e47bbc7029f9bcb701c56498076465b603293
Debora Del Vecchio <d.delvecchio@kevopartners.com>
2020-11-20 11:47:37 +0100

    added currencies constants

---
+++ lib/app/settings/currencies.dart
@@ -0,0 +1,17 @@
+const kCryptoCurrencies = [
+  'bitcoin',
+  'ethereum',
+  'litecoin',
+  'binancecoin',
+  'bitcoin-cash',
+];
+
+const kCurrencies = [
+  'USD',
+  'EUR',
+  'GBP',
+  'JPY',
+  'AUD',
+  'CHF',
+  'CNY',
+];
